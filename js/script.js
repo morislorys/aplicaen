f864b0002c75d77e5f2c0b7db39cf6f05a313a45
morislorys <morislorys@gmail.com>
2023-03-18 21:59:17 +0100

    calm down

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
-let sections = document.querySelectorAll('section');
+/* TOOGLE MENUE*/ 
+let menuIcon =  document.querySelector('#menuIcon');
+let navBar =  document.querySelector('.navBar');
+let  toggleState = 0;
+menuIcon.onclick = () => {
+    if (toggleState == 0){
+        menuIcon.src = "../img/x-regular-24.png";
+        navBar.classList.add("close");
+        toggleState = 1;
+        return
+    }
+    navBar.classList.remove("close");
+    menuIcon.src = "../img/menu-regular-24.png";
+    toggleState = 0;
+};
+
+
+
+
+/* */
+
+ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    sections.forEach(sec=> {
+    /* sections.forEach(sec=> {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
@@ -14,6 +35,11 @@ window.onscroll = () => {
                 document.querySelector('header nav a [href*= ' + id + ']').classList.add('active');
             });
         };
-    });
+    }); */
 
-};
+
+    /* Toogle menu remove navBar when scrool down to sections*/
+    navBar.classList.remove("close");
+    menuIcon.src = "../img/menu-regular-24.png";
+    toggleState = 0;
+}; 
